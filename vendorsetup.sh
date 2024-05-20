bd7929a010e42ce6ff39d9f4fc0713eb0917694c
Pranav Talmale <pranavtalmale@gmail.com>
2024-05-20 10:05:30 +0000

    davinci: Add some OrangeFox build vars
    helliscloser: Checkout from https://github.com/AMWolfstein/twrp_device_xiaomi_davinci/commit/5cce7b2e9fe1745d57432ba1aa32e3b206555e43 and edit for fresh bringup.
    
    Co-Authored-By: Amir <1644943+pwnrazr@users.noreply.github.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,50 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2019-2022 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+FDEVICE="davinci"
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep -w \"$FDEVICE\")
+   if [ -n "$chkdev" ]; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep -w \"$FDEVICE\")
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   echo "** WARNING **: Always set FOX_BUILD_DEVICE to the device codename before starting to build for any device!"
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+	export OF_STATUS_INDENT_LEFT="48"
+	export OF_STATUS_INDENT_RIGHT="48"
+	export OF_SCREEN_H=2340
+	export TARGET_DEVICE_ALT="davinciin"
+  export OF_USE_LEGACY_BATTERY_SERVICES=1
+
+
+
+    # Maintainer Stuff
+    export OF_MAINTAINER="Rumais"
+
+
+fi
